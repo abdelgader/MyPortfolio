c0ce4b00a0aa3bef52f27299ebfca6e442e1defe
Abdalgader Hatem <42221163+abdelgader@users.noreply.github.com>
2022-04-14 16:54:18 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MyPortfolio.Models;
+using MyPortfolio.Models.Repositories;
+
+namespace MyPortfolio
+{
+    public class Startup
+    {
+        private readonly IConfiguration config;
+
+        public Startup(IConfiguration config)
+        {
+            this.config = config;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddScoped<IMyPortfolioRepository<Contactor>, ContactorDbRepository>();
+            services.AddScoped<IMyPortfolioRepository<Project1>, ProjectDbRepository>();
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<MyPortfolioDbContext>();
+            services.AddDbContext<MyPortfolioDbContext>(options =>
+            {
+                options.UseSqlServer(config.GetConnectionString("SqlCon"));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                loggerFactory.AddConsole();
+                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+            }
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+            app.UseAuthentication();
+           
+        }
+    }
+}
